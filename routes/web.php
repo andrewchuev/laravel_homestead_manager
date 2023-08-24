91fae5097408430ba12c4aef60a5e4575ec65805
Andrew A. Chuev <andrew.chuev@gmail.com>
2023-08-24 16:27:18 +0300

    Add bootstrap css

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource( '/config',  HomesteadConfigController::class );
 
-//Route::get('/config/{key}/edit', 'HomesteadConfigController@edit')->name('config.edit');
-//Route::put('/config/{key}', 'HomesteadConfigController@update')->name('config.update');
+
